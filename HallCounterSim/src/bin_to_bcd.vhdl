44acb90cb81b8866eb3e42e69d9fc3a33f7f3414
BrotherAntone <lantonevich@yahoo.com>
2025-11-09 22:26:58 -0500

    New bcd_to_bin Method

--- HallCounterSim/src/bin_to_bcd.vhdl
+++ HallCounterSim/src/bin_to_bcd.vhdl
@@ -13,16 +13,11 @@ END ENTITY bin_to_bcd;
 
 ARCHITECTURE rtl OF bin_to_bcd IS
 
-    SIGNAL s_rpm_unsigned : UNSIGNED (13 DOWNTO 0);
-    SIGNAL s_rem_thous : UNSIGNED (3 DOWNTO 0);
-    SIGNAL s_rem_hund : UNSIGNED (3 DOWNTO 0);
-    SIGNAL s_rem_tens : UNSIGNED (3 DOWNTO 0);
-    SIGNAL s_rem_ones : UNSIGNED (3 DOWNTO 0);
-
-    CONSTANT DIVISOR_THOUS : UNSIGNED(3 DOWNTO 0) := "1010";
-    CONSTANT DIVISOR_HUND : UNSIGNED();
-    CONSTANT DIVISOR_TENS : UNSIGNED();
-    CONSTANT DIVISOR_ONES : UNSIGNED(3 DOWNTO 0) := "1010"; -- Constant d10
+    SIGNAL s_rpm_unsigned : unsigned(i_bin);
 
 BEGIN
+    
+
+
+    
 END ARCHITECTURE rtl;
